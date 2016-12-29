0a33361c8e82ba407c2ceeded1d426cc9e858e4a
Stefan Sundin <git@stefansundin.com>
2016-12-28 23:51:08 -0800

    Make Firefox-specific content_script.js because Firefox won't support async/await until version 52. Also devicePixelRatio behaves differently for some reason.

--- make-xpi-mac.sh
+++ make-xpi-mac.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -ex
-cp -r extension/{js,img,css,*.html} firefox
+cp -r extension/{img,css,*.html} firefox
+cp extension/js/{background,popup}.js firefox/js/
 
 V=$(cat firefox/manifest.json | grep '"version"' | grep -o "\d*\.\d*\.\d*")
 rm -f "one-click-screenshot-$V.xpi"
